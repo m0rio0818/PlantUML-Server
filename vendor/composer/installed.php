94b5e17fc586296db80621a2fc07f747554761a0
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-03-27 11:47:01 +0900

    phpのencodeを追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9b1ddb79c105c1f72fe8de387d1f617dfac139c9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9b1ddb79c105c1f72fe8de387d1f617dfac139c9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jawira/plantuml-encoding' => array(
+            'pretty_version' => 'v1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => 'cec097dfc062e7e13655d9594eacce5f9df639ad',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jawira/plantuml-encoding',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
