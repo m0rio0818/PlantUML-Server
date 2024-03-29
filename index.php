0330b3b5b3aefa102e059c7b010d67ad3198b313
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-03-29 13:10:18 +0900

    ボタンの追加

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 require_once "./pages/getProblems.php";
-
-use function Jawira\PlantUml\encodep;
-
 ?>
 
 <!DOCTYPE html>
@@ -25,12 +22,12 @@ use function Jawira\PlantUml\encodep;
 </head>
 
 <body>
-    <?php $id ?>
+    <?php $id ?>fetch
     <div class="d-flex flex-col h-screen ">
         <div class="d-flex font-medium text-center text-gray-500 underline-b">
             <ul class="flex flex-wrap justify-end -mb-px">
                 <li id="editor" value="on" class="me-2 border-b-2 border-green-500">
-                    <a href="../index.php" class="inline-block p-4 rounded-t-lg text-gray-500 hover:text-black hover:border-gray-300" style="text-decoration:none;">
+                    <a href="../index.php" class="inline-block p-4 rounded-t-lg text-green-500 hover:border-gray-300" style="text-decoration:none;">
                         Editor
                     </a>
                 </li>
@@ -44,21 +41,27 @@ use function Jawira\PlantUml\encodep;
         <h1 class="text-center">
             PlantUML Editor
         </h1>
+        <div>
+            <div class="p-3 flex items-center justify-center sticky top-0 bg-white">
+                <button id="png" value="on" onclick="png_click()" class="bg-transparent mx-1 py-1 px-3 font-semibold border rounded hover:bg-white hover:text-black  hover:border-gray-400 text-white bg-gray-400 border-gray-400 hover:border-transparent">
+                    png
+                </button>
+                <button id="svg" value="off" onclick="svg_click()" class="bg-transparent mx-1 py-1 px-3 font-semibold border rounded hover:bg-gray-400 hover:text-white text-gray-700 border-gray-400 hover:border-transparent">
+                    svg
+                </button>
+                <button id="text" value="off" onclick="text_click()" class="bg-transparent mx-1 font-semibold py-1 px-3 border rounded hover:bg-gray-400 hover:text-white text-gray-700 border-gray-400 hover:border-transparent">
+                    text
+                </button>
+                <button id="download" class="bg-transparent mx-1 font-semibold py-1 px-3 border rounded hover:bg-gray-400 text-gray-700  hover:text-white  border-gray-400 hover:border-transparent">
+                    Download
+                </button>
+            </div>
+        </div>
         <div class="prose flex flex-col h-full lg:flex-row px-5 lg:px-5 py-5 lg:py-2">
             <div id="editor-container" class="w-full lg:w-1/2 h-1/2 md:h-3/4 lg:h-full" style="border:1px solid grey">
             </div>
             <div class="w-full lg:w-1/2 h-1/2 md:h-3/4 lg:h-full overflow-scroll" style="border:1px solid grey">
-                <div class="p-3 flex items-center justify-center sticky top-0 bg-white">
-                    <button id="png" value="on" class="bg-transparent mx-1 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
-                        png
-                    </button>
-                    <button id="svg" value="off" class="bg-transparent mx-1 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
-                        svg
-                    </button>
-                    <button id="text" value="on" class="bg-transparent mx-1 font-semibold py-1 px-2 border hover:bg-blue-500 text-blue-700  hover:text-white  border-blue-500 hover:border-transparent rounded">
-                        text
-                    </button>
-                </div>
+
                 <div id="converted-ele" class="p-2">
                 </div>
             </div>
@@ -67,6 +70,6 @@ use function Jawira\PlantUml\encodep;
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs/loader.min.js"></script>
 <script src="../src/js/problem_page.js"></script>
-<script type="module" src="../src/js/main.js"></script>
+<script src="../src/js/main.js"></script>
 
 </html>
