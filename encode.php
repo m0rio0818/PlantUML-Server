1ad3cace9e494a708ff14dc3c7a6d260bd2f0b2a
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-04-02 10:35:40 +0900

    text変換ロジックの追加

--- encode.php
+++ encode.php
@@ -7,5 +7,14 @@ $data = json_decode(file_get_contents("php://input"), true);
 $value = $data["value"];
 $type = $data["type"];
 
-$encode = encodep($value);
-echo "http://www.plantuml.com/plantuml/{$type}/{$encode}";
+if ($type == "png" || $type == "svg") {
+    $encode = encodep($value);
+    echo "http://www.plantuml.com/plantuml/{$type}/{$encode}";
+} else {
+    $encode = encodep($value);
+    $apiUrl = "https://www.plantuml.com/plantuml/txt/{$encode}";
+    $asciiArt = file_get_contents($apiUrl);
+
+    // 取得したASCIIアートをHTMLに表示
+    echo '<pre>' . $asciiArt . '</pre>';
+}
